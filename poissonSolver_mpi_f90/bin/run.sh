ac68e48cd57c0a155bc9b220ec2c5abd7b12a07c
theparvezahmad <theparvezahmad@outlook.com>
2023-10-12 13:11:16 +0530

    Save

--- poissonSolver_mpi_f90/bin/run.sh
+++ poissonSolver_mpi_f90/bin/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "====================================="
-echo "      Poisson Solver: MPI | C++      "
+echo "    Poisson Solver: MPI | Fortran    "
 echo "====================================="
 echo "Clean Rebuild"
 (cd ../build && make clean)
 (cd ../build && make)
 echo "====================================="
 echo "Starting execution using 8 procs"
-(cd ../build && mpirun -np 8 ./mpi.x)
+(cd ../build && mpiexec -np 8 ./mpi.x)
 # nohup ./source/fsi3l1.x &
 # gnuplot script.gnu
